d31d9b9fe82b627fc2ca8af30f013879ae966082
JunGiKim <junhardstudy@knu.ac.kr>
2021-10-05 17:55:19 +0900

    permission check fixed

--- Application_part/project_kotlin_version/Kotlin_ver/app/src/main/java/com/example/kotlin_ver/MainActivity.kt
+++ Application_part/project_kotlin_version/Kotlin_ver/app/src/main/java/com/example/kotlin_ver/MainActivity.kt
@@ -20,11 +20,14 @@ import android.view.Menu
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
 import java.util.*
+import java.util.jar.Manifest
 import kotlin.collections.ArrayList
 import kotlin.properties.Delegates
 
@@ -35,6 +38,31 @@ class MainActivity : AppCompatActivity(){
     private lateinit var mBLEList : ArrayList<BluetoothDevice>
     private lateinit var mBLEDeviceAdapter : BLEDataAdapter
     private val REQUEST_ENABLE_BT = 1
+    private val REQUEST_MULTIPLE_PERMISSION = 2
+    private val requestPermission = arrayOf(
+        android.Manifest.permission.BLUETOOTH,
+        android.Manifest.permission.BLUETOOTH_ADMIN,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+
+    val mScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            mBLEDeviceAdapter.addDevice(result.device)
+            mBLEDeviceAdapter.notifyDataSetChanged()
+        }
+
+        override fun onBatchScanResults(results: List<ScanResult>) {
+            super.onBatchScanResults(results)
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            Log.i("확인용", "에러 호출!")
+            Log.i("확인용", errorCode.toString())
+            super.onScanFailed(errorCode)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +83,16 @@ class MainActivity : AppCompatActivity(){
 
     override fun onResume() {
         super.onResume()
+        
+        while(true){
+            val rejectedPermissionList = permissionCheck()
+            if(rejectedPermissionList.size > 0){
+                requestPermission(rejectedPermissionList)
+            }
+            else{
+                break
+            }
+        }// 필요한 권한을 모두 허용한 경우에, 앱의 기능 호출
 
         // activity가 resume 될때마다, 사용자가 BL 기능을 off 했는지 확인해서, off 되어 있다면 on 시키도록 intent를 날려줌
         if(!mBLEadapter.isEnabled){
@@ -113,57 +151,51 @@ class MainActivity : AppCompatActivity(){
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    //todo : 이건 뭔지 모르겟음
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_MULTIPLE_PERMISSION -> {
+                if(grantResults.size > 0){
+                    for(item in grantResults){
+                        if(item != PackageManager.PERMISSION_GRANTED)
+                            Toast.makeText(this, "we need permission to start this application", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     private fun scanLeDevice(enable: Boolean) {
         val filters: MutableList<ScanFilter> = java.util.ArrayList()
         val myUUID = UUID.fromString("00000ffff-0000-1000-8000-00805f9b34fb")
         val filter = ScanFilter.Builder().setServiceUuid(ParcelUuid(myUUID)).build()
         //filter = new ScanFilter.Builder().setDeviceAddress("80:6F:B0:90:02:3C").build();
         //filter = new ScanFilter.Builder().setDeviceName("HSP2_2.7.0.1").build();
+
         filters.add(filter)
+
         val scanSettings = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
-        /*
-        if(enable){
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    mBLEadapter.stopLeScan(mLeScanCallback);
-                    invalidateOptionsMenu();
-                }
-            }, SCAN_PERIOD);
-
-            mScanning = true;
-            //mBLEadapter.startLeScan(mLeScanCallback);
-            //mBLEadapter.getBluetoothLeScanner().startScan(filters, scanSettings, mScanCallback);
-
-        }
-
- */
-
         mBLEadapter.bluetoothLeScanner.startScan(filters, scanSettings, mScanCallback)
-        //mBLEadapter.getBluetoothLeScanner().startScan(mScanCallback);
         invalidateOptionsMenu()
     }
-    val mScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            mBLEDeviceAdapter.addDevice(result.device)
-            mBLEDeviceAdapter.notifyDataSetChanged()
-            Log.i("확인용", "밖에 호출!")
-        }
 
-        override fun onBatchScanResults(results: List<ScanResult>) {
-            Log.i("확인용", "배치 호출!")
-            super.onBatchScanResults(results)
+    private fun permissionCheck() : ArrayList<String>{
+        val rejectedPermission = ArrayList<String>()
+        for (permission in requestPermission){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                rejectedPermission.add(permission)
+            }
         }
-
-        override fun onScanFailed(errorCode: Int) {
-            Log.i("확인용", "에러 호출!")
-            Log.i("확인용", errorCode.toString())
-            super.onScanFailed(errorCode)
-        }
+        return rejectedPermission
     }
+
+    private fun requestPermission(requiredPermissionList : ArrayList<String>){
+        ActivityCompat.requestPermissions(this, requestPermission, REQUEST_MULTIPLE_PERMISSION)
+    }
+
 
 
 
